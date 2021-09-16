6d03954957bbeffc761677befa0ec40cc91fbdaa
saulobt <saulobt@gmail.com>
2021-09-16 14:41:25 -0300

    Implementação Metodos da Classe  ListaEncadeada; add()

--- src/main/java/com/projeto/listaencadeada/ListaEncadeada.java
+++ src/main/java/com/projeto/listaencadeada/ListaEncadeada.java
@@ -7,19 +7,19 @@ public class ListaEncadeada<T> {
         this.referenciaEntrada = null;
     }
 
-    public boolean isEmpty(){
-         return referenciaEntrada == null ? true :false;
+    public boolean isEmpty() {
+        return referenciaEntrada == null ? true : false;
     }
 
-    public int size(){
-        int tamanhoLista=0;
+    public int size() {
+        int tamanhoLista = 0;
 
         No<T> referenciaAuxiliar = referenciaEntrada;
 
-        while (true){
-            if (referenciaAuxiliar != null){
-                tamanhoLista ++;
-                if (referenciaAuxiliar.getProximoNo() != null){
+        while (true) {
+            if (referenciaAuxiliar != null) {
+                tamanhoLista++;
+                if (referenciaAuxiliar.getProximoNo() != null) {
                     referenciaAuxiliar = referenciaAuxiliar.getProximoNo();
                 } else {
                     break;
@@ -30,5 +30,22 @@ public class ListaEncadeada<T> {
         }
 
         return tamanhoLista;
+    }
+
+    public void add(T conteudo) {
+        No<T> novoNo = new No<>();
+        if (this.isEmpty()) {
+            referenciaEntrada = novoNo;
+            return;
+        }
+
+        No<T> noAuxiliar = referenciaEntrada;
+
+        for (int i = 0; i < this.size() - 1; i++) {
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+
+        noAuxiliar.setProximoNo(novoNo);
+
     }
 }
