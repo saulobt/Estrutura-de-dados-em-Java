181d9c34e8e00d4d49a2e783a1dbdef707e5772b
saulobt <saulobt@gmail.com>
2021-09-30 00:47:45 -0300

    implementando medoto Main() e testando

--- src/main/java/com/projeto/listaduplamenteencadeada/Main.java
+++ src/main/java/com/projeto/listaduplamenteencadeada/Main.java
@@ -1,7 +1,21 @@
 package com.projeto.listaduplamenteencadeada;
 
+import com.projeto.listaencadeada.ListaEncadeada;
+
 public class Main {
     public static void main(String[] args) {
-
+        ListaDuplamenteEncadeada<String> myListDuplamenteEncadeada = new ListaDuplamenteEncadeada<>();
+        myListDuplamenteEncadeada.add("c1");
+        myListDuplamenteEncadeada.add("c2");
+        myListDuplamenteEncadeada.add("c3");
+        myListDuplamenteEncadeada.add("c4");
+        myListDuplamenteEncadeada.add("c5");
+        myListDuplamenteEncadeada.add("c6");
+        myListDuplamenteEncadeada.add("c7");
+        System.out.println(myListDuplamenteEncadeada);
+        myListDuplamenteEncadeada.remove(3);
+        myListDuplamenteEncadeada.add(3, "99");
+        System.out.println(myListDuplamenteEncadeada);
+        System.out.println(myListDuplamenteEncadeada.get(3));
     }
 }
