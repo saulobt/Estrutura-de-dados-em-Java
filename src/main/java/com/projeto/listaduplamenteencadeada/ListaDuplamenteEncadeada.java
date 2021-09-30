51decd39843fe68bb2c01c69553ea0f900b23c25
saulobt <saulobt@gmail.com>
2021-09-29 23:49:53 -0300

    implementando medoto add(index) sobregarregado.

--- src/main/java/com/projeto/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
+++ src/main/java/com/projeto/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
@@ -12,7 +12,37 @@ public class ListaDuplamenteEncadeada {
 
     }
 
-    public int size(){
+    public int size() {
         return tamanhoLista;
+    }
+
+    private NoDuplo<T> getNo(int index) {
+        NoDuplo<T> noAuxiliar = primeiroNo;
+        for (int i = 0; (i < index) && noAuxiliar != null; i++) {
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        return noAuxiliar;
+    }
+
+    public T get(int index) {
+        return this.getNo(index).getConteudo();
+    }
+
+    public void add(T elemento) {
+        NoDuplo<T> novoNo = new NoDuplo<T>(elemento);
+        novoNo.setProximoNo(null);
+        novoNo.setNoAnterior(ultimoNo);
+        if (primeiroNo == null) {
+            primeiroNo = novoNo;
+        }
+        if (ultimoNo != null) {
+            ultimoNo.setProximoNo(novoNo);
+        }
+        ultimoNo = novoNo;
+        tamanhoLista++;
+    }
+
+    public void add(int index, T Element) {
+
     }
 }
